d3c5fa11cbe556eb068d6d6c93c0b0e7d12cf84f
kromych <kromych@noreply.users.github.com>
2021-04-18 14:55:05 -0700

    VS project to build Lua

---
+++ vs/luaw/luaw.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+
+#include "../../lua.c"
+
+int WINAPI wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	PWSTR pCmdLine,
+	int nCmdShow)
+{
+
+}
